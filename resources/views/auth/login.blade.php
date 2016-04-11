36962dd6d9e50de2a9f2ba0b711fa5f9e2de2e9b
neil <neeil@mail.ru>
2016-04-11 18:19:52 +0300

    no message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,8 +58,6 @@
                                     Registration
                                 </a>
 
-
-
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
                             </div>
                         </div>
