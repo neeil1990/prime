9cb3d1f823195472cdd8ed2a8262ef2364817e08
neil <neeil@mail.ru>
2016-05-19 13:02:37 +0300

    no message

--- resources/views/page/edit_pass_seo.blade.php
+++ resources/views/page/edit_pass_seo.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Добавить пароли контекста</div>
+                    <div class="panel-heading">Изменить пароли SEO</div>
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/update-create-pass-seo') }}">
                             {!! csrf_field() !!}
