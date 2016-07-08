cdefef85b6287f94233d42cfb863f488e44caa08
neil <neeil@mail.ru>
2016-07-08 14:30:50 +0300

    no message

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,10 @@
 
 @section('content')
 
+
     <section class="content">
-
-        Привет!
-
-
+        <div id="topvisor_GPr"></div>
     </section>
 
+    <script type="text/javascript" src="https://topvisor.ru/js/widget/apometr/apometr.php?region_action=1&searcher=0&region_key=213&div_id=topvisor_GPr&charset=utf-8&lang=ru"></script>
     @stop
