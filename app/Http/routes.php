0144c6b7fa44c6426bfeb4eb82ecbe500cc78d61
neil <neeil@mail.ru>
2016-08-23 12:55:54 +0300

    no message

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -57,32 +57,21 @@ Route::get('/get-seranking-sum', function()
 
             $setting_payouts = \DB::table('setting_payouts')->where('id',1)->first();
 
-            if($p->procent_bonus == 0){
+            if($p->procent_bonus == 0 and $p->count_day_fine == 0 and $p->procent_fine == 0){
+                $enddate = strtotime('+' . $setting_payouts->count_day_fine . ' day', strtotime(preg_replace('~^(\d+)\/(\d+)\/(\d+)$~', '$3/$2/$1', $p->end)));
                 if($sum_osvoen_procent > $setting_payouts->procent_bonus){
                    $procent_bonus = $p->budget/100*$p->procent_seo;
+                }elseif(strtotime("now") > $enddate and $sum_osvoen_procent < $setting_payouts->procent_fine){
+                    $procent_bonus = '-' . $p->budget / 100 * $p->procent_seo;
                 }
-            }elseif($p->procent_bonus){
-                if($sum_osvoen_procent > $p->procent_bonus){
-                    $procent_bonus = $p->budget/100*$p->procent_seo;
-                }
-            }
-            elseif($p->count_day_fine == 0 and $p->procent_fine == 0){
-                    $enddate = strtotime('+' . $setting_payouts->count_day_fine . ' day', strtotime(preg_replace('~^(\d+)\/(\d+)\/(\d+)$~', '$3/$2/$1', $p->end)));
-                if (strtotime("now") > $enddate and $sum_osvoen_procent < $setting_payouts->procent_fine) {
-                        $procent_bonus = '-' . $p->budget / 100 * $p->procent_seo;
-                    }
-
-            }else{
+            }else {
                 $enddate = strtotime('+' . $setting_payouts->count_day_fine . ' day', strtotime(preg_replace('~^(\d+)\/(\d+)\/(\d+)$~', '$3/$2/$1', $p->end)));
-                if(strtotime("now") > $enddate and $sum_osvoen_procent < $p->procent_fine){
-                    $procent_bonus = '-'.$p->budget/100*$p->procent_seo;
-                }else{
-                    $procent_bonus = 0;
+                if ($sum_osvoen_procent > $p->procent_bonus) {
+                    $procent_bonus = $p->budget / 100 * $p->procent_seo;
+                } elseif (strtotime("now") > $enddate and $sum_osvoen_procent < $p->procent_fine) {
+                    $procent_bonus = '-' . $p->budget / 100 * $p->procent_seo;
                 }
             }
-
-
-
 
             \DB::table('project_seos')->where('id', $p->id)
                 ->update(array(
